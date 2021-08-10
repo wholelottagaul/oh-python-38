3bd9632096e59fb419a2954d44c4127d73008690
Shawn Gaul <shgaul@microsoft.com>
2021-08-10 22:41:57 +0000

    Add validation for rating bits

--- CreateRating/__init__.py
+++ CreateRating/__init__.py
@@ -3,6 +3,7 @@ import json
 import uuid
 from datetime import datetime
 import azure.functions as func
+import requests
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -14,17 +15,31 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         pass
 
     if rating:
-        rating.id = str(uuid.uuid4())
-        rating.timestamp = datetime.now()
+        if not validateForeignKey("https://serverlessohapi.azurewebsites.net/api/GetProduct", {'productId': rating['productId']}):
+            return func.HttpResponse(
+                "Invalid Product Id",
+                status_code=400
+            )
+        
+        if not validateForeignKey("https://serverlessohapi.azurewebsites.net/api/GetUser", {'userId': rating['userId']}):
+            return func.HttpResponse(
+                "Invalid User Id",
+                status_code=400
+            )
+            
+        rating['id'] = str(uuid.uuid4())
+        rating['timestamp'] = datetime.now()
 
         return func.HttpResponse(
-             json.dumps(rating),
-             status_code=200
-        )
-        return 
+            json.dumps(rating),
+            status_code=200
+        )            
     else:
         return func.HttpResponse(
              "No rating object found",
              status_code=400
         )
 
+def validateForeignKey(url, params):
+    result = requests.get(url, params=params)
+    return result.status_code == 200        
